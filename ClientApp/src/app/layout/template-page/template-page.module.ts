1b16815c2cefe7336acd68a6908230fd56a589a0
Andreas Klapperich <andreas@MacBook003.fritz.box>
2018-03-05 20:30:34 +0100

    ChartModule and FormModule added  to the template

--- ClientApp/src/app/layout/template-page/template-page.module.ts
+++ ClientApp/src/app/layout/template-page/template-page.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule as Ng2Charts } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
 
 import { TemplatePageRoutingModule } from './template-page-routing.module';
 import { TemplatePageComponent } from './template-page.component';
 import { PageHeaderModule } from './../../shared';
 
 @NgModule({
-    imports: [CommonModule, TemplatePageRoutingModule, PageHeaderModule],
+    imports: [CommonModule, Ng2Charts, FormsModule,
+        TemplatePageRoutingModule, PageHeaderModule],
     declarations: [TemplatePageComponent]
 })
 export class TemplatePageModule {}
